f4eccf203eb8d362fb3183012ffcb076043af048
zellwwf <zellwwf@gmail.com>
2025-03-11 18:27:44 -0400

    Adjust a few hyperlinks to make them go to the correct places. Remove older imports and un-used code. Testing out how i will place all the concepts we use

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,8 +1,6 @@
 import styles from './styles.module.css';
 import { HomepageCardSection, Card } from '../HomepageCardSection';
 import allocinit_icon from '@site/static/img/icon.png';
-import SmartContracts from '@site/static/img/nil/nil-icons/smart_contracts.png';
-import CLI from '@site/static/img/nil/nil-icons/cli.png';
 import Essentials from '@site/static/img/nil/nil-icons/essentials.png'
 
 const BRAND = '[[alloc] init]'
@@ -10,10 +8,9 @@ const HOMEPAGE_HEADER_STRING = `${BRAND} Documentation`;
 const HOMEPAGE_HEADER_SUBTITLE = `${BRAND} is adding Zero-Knowledge-Proofs to Bitcoin, the hard way, without any forks!`;
 
 const CARDS = <HomepageCardSection>
-  <Card icon={<img src={allocinit_icon} />} id='alloc101' title='[[alloc] init] 101' description='Our current research objectives and stack' to='/alloc_init/architecture' />
+  <Card icon={<img src={allocinit_icon} />} id='alloc101' title='[[alloc] init] 101' description='What is [[alloc] init]' to='/alloc_init/' />
+  <Card icon={<img src={Essentials} />} id='alloc101' title='Architecture' description='Our current architecture' to='/alloc_init/architecture' />
   <Card icon={<img src={Essentials} />} id='manifesto' title='Manifesto' description='The Philosophy behind' to='/alloc_init/manifesto' />
-  {/*<Card icon={<img src={SmartContracts} />} id='smart_contracts' title='Tutorials' description='Write, deploy, and call smart contracts' to='/nil/getting-started/working-with-smart-contracts/writing-a-contract' />*/}
-  {/*<Card icon={<img src={CLI} />} id='cli' title='=nil; tools' description='Configure and use the key developer tools' to='/nil/tools/nil-cli/usage' />*/}
 </HomepageCardSection>
 
 export default function HomepageNilProducts() {
